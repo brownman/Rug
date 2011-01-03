962fb2ea7bdb0cfe5fd958c3f1f207288c494fef
Rob <rob.s.brit@gmail.com>
2011-01-03 18:19:46 -0500

    Fixed up some includes for Windows.

--- lib/Rug.rb
+++ lib/Rug.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../ext/Rug.so'
+require File.dirname(__FILE__) + (RUBY_PLATFORM =~ /win32/ ? "/../bin/Rug.dll" : '/../ext/Rug.so')
 require File.dirname(__FILE__) + '/Animation'
 require File.dirname(__FILE__) + '/Colour'
 require File.dirname(__FILE__) + '/HasAnimation'
