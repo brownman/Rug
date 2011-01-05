7c18929195d3d3d97020c2c8a8dfcbee03b4eddf
Rob <rob.s.brit@gmail.com>
2011-01-04 21:30:55 -0500

    Added intro example.

---
+++ examples/lesson1/introduction.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + "/../../lib/Rug"
+
+# Configuration settings for our window:
+# - put a title for our window
+# - make the window 1024x768 pixels
+Rug.conf do
+  title  "Lesson 1 - Introduction to Rug"
+  width  1024
+  height 768
+end
+
+Rug.draw do
+  # Draw some text on the screen
+  text 10, 10, "Hello, world!"
+end
+
+Rug.start
